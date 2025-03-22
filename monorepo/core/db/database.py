31d8c23c880de6ca61302118df4b8cac31c07e8a
m0erd <m0er@outlook.com>
2025-03-22 16:36:17 -0400

    db

--- monorepo/core/db/database.py
+++ monorepo/core/db/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.exc import OperationalError
 import os
 
 db_password = os.getenv("DB_PASSWORD")
-DATABASE_URL = os.getenv("DATABASE_URL", f"postgresql://user:{db_password}@postgres/fastapi_db")
+DATABASE_URL = os.getenv("DATABASE_URL", f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}")
 
 engine = create_engine(DATABASE_URL, echo=True)
 
